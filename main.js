256b14800f2654d8c922342280fde2a26c0b9e86
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2023-04-08 02:21:06 +0100

    actions: Add support for running test-command in container.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const core = require("@actions/core");
 const compose = require("docker-compose");
 const utils = require("./utils");
+const path = require("path");
 
 try {
   const composeFiles = utils.parseComposeFiles(
@@ -31,6 +32,24 @@ try {
     .catch((err) => {
       core.setFailed(`compose up failed ${JSON.stringify(err)}`);
     });
+
+  // Run tests command.
+
+  const testContainer = core.getInput("test-container");
+  const testCommand = core.getInput("test-command");
+  if (testCommand && testContainer) {
+    const test = compose.exec(testContainer, testCommand, {
+      cwd: path.join(__dirname),
+    });
+
+    test
+      .then(() => {
+        console.log("tests passed");
+      })
+      .catch((err) => {
+        core.setFailed(`tests failed ${JSON.stringify(err)}`);
+      });
+  }
 } catch (error) {
   core.setFailed(error.message);
 }
